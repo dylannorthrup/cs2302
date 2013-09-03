45d97d05702d0d1efd819454496a7610b1b7df81
Dylan Northrup <github@doc-x.net>
2013-09-03 19:23:53 -0400

    Test Class

---
+++ code/week 1/src/inheritance/Test.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Test {
+  public static void main(String[] args) {
+	Test t = new Test();
+	// Inherits Object.toString and uses that for the Test object
+	System.out.println(t.toString());
+  }
+
+}
